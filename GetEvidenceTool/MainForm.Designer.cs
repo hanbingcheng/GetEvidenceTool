d426825ec1dd1e3ac9af9a7806a5d453f8f8c013
cheng-hnSKTOP-FTKL8PR\cheng-hn <hanbing.cheng@gmail.com>
2022-04-08 18:17:31 +0900

    #v1.0

---
+++ GetEvidenceTool/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace GetEvidenceTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.console = new System.Windows.Forms.RichTextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnLocation = new System.Windows.Forms.Button();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(657, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 55);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // console
+            // 
+            this.console.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.console.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.console.Location = new System.Drawing.Point(12, 157);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(780, 138);
+            this.console.TabIndex = 1;
+            this.console.Text = "";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Location = new System.Drawing.Point(657, 88);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(109, 52);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtFolderName
+            // 
+            this.txtFolderName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolderName.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtFolderName.Location = new System.Drawing.Point(263, 108);
+            this.txtFolderName.Name = "txtFolderName";
+            this.txtFolderName.Size = new System.Drawing.Size(290, 32);
+            this.txtFolderName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(32, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Evidence Folder Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Evidence Location";
+            // 
+            // btnLocation
+            // 
+            this.btnLocation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLocation.Font = new System.Drawing.Font("Yu Gothic UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLocation.Location = new System.Drawing.Point(478, 53);
+            this.btnLocation.Name = "btnLocation";
+            this.btnLocation.Size = new System.Drawing.Size(75, 31);
+            this.btnLocation.TabIndex = 5;
+            this.btnLocation.Text = "・・・";
+            this.btnLocation.UseVisualStyleBackColor = true;
+            this.btnLocation.Click += new System.EventHandler(this.btnLocation_Click);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.AcceptsTab = true;
+            this.txtLocation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLocation.Font = new System.Drawing.Font("Yu Gothic UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLocation.Location = new System.Drawing.Point(32, 53);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(437, 31);
+            this.txtLocation.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 315);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.btnLocation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolderName);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private RichTextBox console;
+        private Button btnStop;
+        private TextBox txtFolderName;
+        private Label label1;
+        private Label label2;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button btnLocation;
+        private TextBox txtLocation;
+    }
+}
---
+++ GetEvidenceTool/MainForm.cs
@@ -0,0 +1,284 @@
+using Npgsql;
+using System.Configuration;
+using System.Text;
+
+namespace GetEvidenceTool
+{
+    public partial class MainForm : Form
+    {
+        private string datetimeFormat = "yyyy/MM/dd HH:mm:ss";
+        private string loggEncoding = string.Empty;
+        // db connection setting
+        private bool isExportTable = false;
+        private string connString = string.Empty;
+        private string nullString = string.Empty;
+
+        private string startTime = string.Empty;
+        private List<string> logs = new List<string>();
+        private string location = string.Empty;
+        private string folderName = string.Empty;
+        private string rootDir = string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.LoadConfig();
+        }
+
+        private void LoadConfig()
+        {
+            this.datetimeFormat = ConfigurationManager.AppSettings["datetimeformat"] ?? "";
+            if (string.IsNullOrEmpty(datetimeFormat))
+            {
+                MessageBox.Show("datetimeFormat is not set on config file");
+                return;
+            }
+            string log1 = ConfigurationManager.AppSettings["log1"] ?? "";
+            if (!String.IsNullOrEmpty(log1) && File.Exists(log1))
+            {
+                this.logs.Add(log1);
+            }
+            string log2 = ConfigurationManager.AppSettings["log2"] ?? "";
+            if (!String.IsNullOrEmpty(log2) && File.Exists(log2))
+            {
+                this.logs.Add(log2);
+            }
+            string log3 = ConfigurationManager.AppSettings["log3"] ?? "";
+            if (!String.IsNullOrEmpty(log3) && File.Exists(log3))
+            {
+                this.logs.Add(log3);
+            }
+            this.loggEncoding = ConfigurationManager.AppSettings["logEncoding"] ?? "UTF-8";
+            if ("true".Equals(ConfigurationManager.AppSettings["isExportTable"]))
+            {
+                this.isExportTable = true;
+                string host = ConfigurationManager.AppSettings["host"] ?? "";
+                string port = ConfigurationManager.AppSettings["port"] ?? "";
+                string user = ConfigurationManager.AppSettings["user"] ?? "";
+                string passwd = ConfigurationManager.AppSettings["password"] ?? "";
+                string dbname = ConfigurationManager.AppSettings["dbname"] ?? "";
+                if (string.IsNullOrEmpty(host) ||
+                    string.IsNullOrEmpty(port) ||
+                    string.IsNullOrEmpty(user) ||
+                    string.IsNullOrEmpty(passwd) ||
+                    string.IsNullOrEmpty(dbname))
+                {
+                    MessageBox.Show("db connection setting is invalid on config file");
+                    return;
+                }
+                this.connString = String.Format("Server={0};Port={1};User Id={2};Password={3};Database={4}", host, port, user, passwd, dbname);
+                this.nullString = ConfigurationManager.AppSettings["nullString"] ?? "";
+            }
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+           if (!this.IsReady())
+            {
+                return;
+            }
+
+            this.btnStart.Enabled = false;
+            this.btnLocation.Enabled = false;
+            this.txtFolderName.ReadOnly = true;
+
+            this.rootDir = Path.Combine(location, folderName);
+            if (Directory.Exists(rootDir))
+            {
+                Directory.Delete(rootDir, true);
+            }
+            Directory.CreateDirectory(rootDir);
+
+            string beforeDir = Path.Combine(rootDir, DateTime.Now.ToString("yyyyMMddHHmmss") + "_before");
+            Directory.CreateDirectory(beforeDir);
+            this.startTime = DateTime.Now.ToString(this.datetimeFormat);
+
+            this.console.Clear();
+            this.OutputToConsole("collect evidence on start");
+            this.ExportTables(beforeDir);
+
+            this.btnStop.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            this.btnStop.Enabled = false;
+
+            string afterDir = Path.Combine(rootDir, DateTime.Now.ToString("yyyyMMddHHmmss") + "_after");
+            Directory.CreateDirectory(afterDir);
+
+            this.OutputToConsole("collect evidence on end");
+            this.CollectLog(afterDir); 
+            this.ExportTables(afterDir); 
+
+            this.btnLocation.Enabled = true;
+            this.txtFolderName.ReadOnly = false;
+            this.btnStart.Enabled = true;
+        }
+
+        private void btnLocation_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "select folder to output evidences";
+            fbd.ShowNewFolderButton = true;
+            DialogResult dr = fbd.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                location = fbd.SelectedPath;
+                this.txtLocation.Text = location;
+            }
+        }
+
+        private bool IsReady()
+        {
+            if (string.IsNullOrEmpty(location))
+            {
+                MessageBox.Show("evidence location is not set.");
+                return false;
+            }
+            string folderName = this.txtFolderName.Text;
+            if (string.IsNullOrEmpty(folderName))
+            {
+                MessageBox.Show("folder name is not set");
+                return false;
+            }
+            if (0 <= folderName.IndexOfAny(Path.GetInvalidPathChars()))
+            {
+                return false;
+            }
+            this.folderName = folderName;
+            return true;
+        }
+
+        private void OutputToConsole(string message)
+        {
+            this.console.AppendText(message);
+            this.console.AppendText(Environment.NewLine);
+        }
+
+        private void CollectLog(string path)
+        {
+            foreach(string logPath in this.logs)
+            {
+                OutputToConsole(String.Format("collect log from: {0}", logPath));
+
+                string filename = Path.GetFileName(logPath);
+                string outputLogPath = Path.Combine(path,filename);
+                string line = string.Empty;
+                bool find = false;
+                using(FileStream fs = new FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using(StreamReader sr = new StreamReader(fs, Encoding.GetEncoding(this.loggEncoding)))
+                using (StreamWriter sw = new StreamWriter(outputLogPath, true, Encoding.GetEncoding(this.loggEncoding)))
+                {
+                    while (-1 < sr.Peek())
+                    {
+                        line = sr.ReadLine() ?? "";
+                        if (find)
+                        {
+                            sw.WriteLine(line);
+                        }
+                        else if (this.startTime.Length < line.Length)
+                        {
+                            string timeStr = line.Substring(0, this.startTime.Length);
+                            if (this.startTime.CompareTo(timeStr) <= 0)
+                            {
+                                find = true;
+                                sw.WriteLine(line);
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void ExportTables(string path)
+        {
+            if (!this.isExportTable)
+            {
+                return;                                           
+            }
+
+            string exportTablesFilePath = Path.Combine(Directory.GetCurrentDirectory(), "export_tables.txt");
+            if(!File.Exists(exportTablesFilePath))
+            {
+                MessageBox.Show("export_tables.txt is not exist on app location.");
+                return;
+            }
+
+            string line = string.Empty;
+
+            using (FileStream fs = new FileStream(exportTablesFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (StreamReader sr = new StreamReader(fs, Encoding.GetEncoding("UTF-8")))
+            {
+                while (-1 < sr.Peek())
+                {
+                    line = sr.ReadLine() ?? "";
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        string[] data = line.Split(":");
+                        if (data.Length == 2)
+                        {
+                            string csvPath = Path.Combine(path, data[0] + ".csv");
+                           this. OutputToConsole(String.Format("export data from table: {0} to {1}.", data[0], csvPath));
+                            this.ExportDataToCSV(csvPath, data[1]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ExportDataToCSV(string filePath, string sql)
+        {
+            using (var con = new NpgsqlConnection(this.connString))
+            {
+                con.Open();
+
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, con);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+                // header
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    builder.Append(dr.GetName(i));
+                    if (i != dr.FieldCount - 1)
+                    {
+                        builder.Append(",");
+                    }
+                }
+                using (StreamWriter sw = new StreamWriter(filePath, true, Encoding.GetEncoding("UTF-8")))
+                {
+                    sw.WriteLine(builder.ToString());
+                    // data
+                    long count = 0;
+                    while (dr.Read())
+                    {
+                        count++;
+                        builder = new StringBuilder();
+                        for (int i = 0; i < dr.FieldCount; i++)
+                        {
+                            if (dr[i] == DBNull.Value)
+                            {
+                                builder.Append(this.nullString);
+                            }
+                            else
+                            {
+                                builder.Append(dr[i]);
+                            }                            builder.Append(dr[i] ?? this.nullString);
+                            if (i != dr.FieldCount - 1)
+                            {
+                                builder.Append(",");
+                            }
+                        }
+                        sw.WriteLine(builder.ToString());
+                    }
+                    this.OutputToConsole(string.Format("exported {0} rows", count));
+                }
+
+            }
+        }
+    }
+}
